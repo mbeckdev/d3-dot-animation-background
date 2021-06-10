7cbeb04e5ae82e0a6b6bcc5f81df087d53c5ecda
mbeckdev <mbeckdev42@gmail.com>
2021-06-10 15:47:16 -0600

    Decided to try another repo with canvas

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # d3-dot-animation-background
+
 An animated background using D3
+
+## Directions
+
+- reload the page to see new art made from D3 dots!
+
+## Original Intent
+
+Ah geez, I thought this would work, and maybe it can. Here's where I'm at:
+
+- Trying to put multiple dots on the page with D3 library = done
+- Trying to make dots move and put lines between them if dots are close,
+  - wait, how do I make things move? Idea1: css transitions, idea2: use canvas
+  - If I use transitions, I'll need to know:
+    - All dot locations so I can draw lines between them
+    - Lines can be little rotated rectangles
+    - starting and end points of all circles and lines - could calculate them probably
+    - It'll have to repeat the transition to make it look cool
+    - Can I create a position at certain times? Sounds like a tick() that might be easier in canvas.
+
+## D3? CSS? canvas?
+
+- Wait, this d3 thing is nice for making circles, but I can see making the shapes in CSS.
+- If I use D3 or CSS I can put this section in more than once, because canvas only is allowed once per page. I was thinking of using this as a header, but it might be sweet to use it more than once one a page.
+
+## Project Status
+
+- I'm going to try this in canvas in a new repo and get that working and maybe come back to CSS or D3 way of doing it.
+- It's a shame that I named this with d3 in the title, (let that be a lesson to me for naming it like that) so I'm going to start a new repository.
+- Enjoy this epic svg art that changes when you reload
--- app.js
+++ app.js
@@ -39,6 +39,14 @@ function maybeMakeCircle(radius, color, x, y) {
   }
 }
 
+function makeLine(x1, y1, x2, y2) {
+  // make line from x1,y1 to x2,y2
+}
+
+let allDots = [];
+//check if dots are close to each other
+//  if so, draw a line between them
+
 function yesOrNo() {
   //returns true or false
   //on whether or not to make something there
@@ -48,3 +56,62 @@ function yesOrNo() {
   }
   return false;
 }
+
+// Section for moving background
+// var width2 = 500;
+// var height2 = 500;
+//Create SVG element
+var svg2 = d3
+  .select('#moving-background')
+  .append('svg')
+  .attr('width', width2)
+  .attr('height', height2);
+svg2
+  .append('circle')
+  .attr('r', '20px')
+  .attr('fill', '#fff')
+  .attr('cx', '20px')
+  .attr('cy', '20px');
+// svg2.transition().duration(1000).style('background-color', 'red');
+
+svg2
+  .append('circle')
+  .attr('r', radius)
+  .attr('fill', color)
+  .attr('cx', x)
+  .attr('cy', y);
+
+let dot = d3
+  .select('.dot')
+  .append('circle')
+  .attr('r', radius)
+  .attr('fill', color)
+  .attr('cx', x)
+  .attr('cy', y);
+
+// .select('.dot')
+// .data(data)
+// .enter()
+// .append('g')
+// .attr('class', 'dot');
+
+// dots
+//   .selectAll('.dot')
+//   .append('circle')
+//   .attr('r', radius)
+//   .attr('fill', color)
+//   .attr('cx', x)
+//   .attr('cy', y);
+
+// .data(data)
+// .enter()
+// .append('circle')
+// .attr('r', 2.5)
+// .attr('cx', function (d) {
+//   return x(d.inspected_at);
+// })
+// .attr('cy', function (d) {
+//   return y(d.flow_data);
+// })
+// .style('fill', 'blue')
+// .style('opacity', '.5');
